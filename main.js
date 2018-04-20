a7be6d384584f7a369dbafadda97b25932d38b99
Matt <MVSucharski@gmail.com>
2018-04-19 23:09:48 -0400

    api call working with input

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ console.log('working');
 const btn = document.querySelector('button');
 const api = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const api_key = '&units=imperial&appid=8ea4a4c502e12757ddbe03749288ce29';
-let weather_api = api + city + api_key;
 
-function getWeather() {
+btn.addEventListener('click', event => {
+    let city = document.querySelector('#city').value;
+    let country = document.querySelector('#country');
+    let weather_api = api + city + api_key;
+    function getWeather() {
     $.ajax({
       method: "GET",
       url: weather_api,
@@ -18,9 +21,6 @@ function getWeather() {
       console.log(json);
   }
 
-btn.addEventListener('click', event => {
-    let city = document.querySelector('#city').value;
-    let country = document.querySelector('#country');
     console.log(city);
     getWeather();
 })
