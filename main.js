1a95f7d493a8d61725ebd0bc2f4ebb10cb5ba000
Matt <MVSucharski@gmail.com>
2018-04-22 23:59:03 -0400

    did some css and got search working

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('working');
 
 const btn = document.querySelector('button');
-const api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const api = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const api_key = '&units=imperial&appid=8ea4a4c502e12757ddbe03749288ce29';
 
 btn.addEventListener('click', event => {
@@ -18,13 +18,24 @@ btn.addEventListener('click', event => {
   
     });
   }
-  function onSuccess(json) {
+    function onSuccess(json) {
       console.log("The current temp is " + json.main.temp + " degrees Fahrenheit");
   }
+    function displayInfo() {
+        const newDiv = document.createElement('div');
+        newDiv.id = 'showWeather';
+        let currentWeather = document.createTextNode("The current temp is " + " degrees Fahrenheit");
+        newDiv.appendChild(currentWeather);
 
+
+    }
     console.log(city);
     getWeather();
-    
+    displayInfo();
 })
 
-
+window.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+        console.log('kool');
+    }
+})
