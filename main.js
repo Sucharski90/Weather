bc2cc21123a1a6dab35040e981115aa06b8bde2b
Matt <MVSucharski@gmail.com>
2018-04-25 03:32:41 -0400

    added focus on page load

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log('working');
 const btn = document.querySelector('button');
 const api = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const api_key = '&units=imperial&appid=8ea4a4c502e12757ddbe03749288ce29';
+const focus = document.querySelector('#city').focus();
 
 btn.addEventListener('click', event => {
     let city = document.querySelector('#city').value;
@@ -36,6 +37,33 @@ btn.addEventListener('click', event => {
 
 window.addEventListener('keydown', e => {
     if (e.keyCode === 13) {
-        console.log('kool');
+    let city = document.querySelector('#city').value;
+    let country = document.querySelector('#country');
+    let weather_api = api + city + api_key;
+
+    function getWeather() {
+    $.ajax({
+      method: "GET",
+      url: weather_api,
+      data: $("form").serialize(),
+      success: onSuccess,
+  
+    });
+  }
+
+    function onSuccess(json) {
+      console.log("The current temp is " + json.main.temp + " degrees Fahrenheit");
+  }
+    function displayInfo() {
+        const newDiv = document.createElement('div');
+        newDiv.id = 'showWeather';
+        let currentWeather = document.createTextNode("The current temp is " + " degrees Fahrenheit");
+        newDiv.appendChild(currentWeather);
+
+
+    }
+    console.log(city);
+    getWeather();
+    displayInfo();
     }
 })
